cef2d05c052d5d19d5a5b50d628a11c3fcedd191
Yuliya Sheludyakova <julia.sheludyakova@gmail.com>
2020-03-03 00:38:12 +0100

    Refactored the show-types pipe

--- frontend/src/app/entries-list/card/show-types-pipe/show-types.pipe.ts
+++ frontend/src/app/entries-list/card/show-types-pipe/show-types.pipe.ts
@@ -4,52 +4,53 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'showTypes'
 })
 export class ShowTypesPipe implements PipeTransform {
-
+  types: any[];
+  origin: string;
+  length: number;
+  concatinated: string;
+  
   transform(types: any[], origin: string): string {
-    let concatTypes: string = '';
+    this.types = types;
+    this.origin = origin;
     if (!types) {
       return '';
     }
-    length = types.length;
-    for(let i = 0; i < length; i++) {
-      if (this.isMoreThanOneMuseumType(origin)) {
-        concatTypes += types[i].type.toLowerCase() + ' & mehr';
-        break;
-      }
-      if (this.isThirdCuisine(i, origin)) {
-        concatTypes += ' & mehr';
-        break;
-      }
-      if (this.isSecondCuisine(i, origin) || this.isLastElement(i, length)) {
-        concatTypes += types[i].type.toLowerCase();
+    this.concatinated = '';
+    this.length = types.length;    
+    if (this.isMuseumCard() || this.isRestaurantCard()) {
+      this.transfromForCard();
+    } else {
+      this.transformForDetails();
+    }
+    return this.concatinated;
+  }
+
+  isMuseumCard(): boolean {
+    return this.origin === 'museumCard';
+  }
+
+  isRestaurantCard(): boolean {
+    return this.origin === 'restaurantCard';
+  }
+
+  transfromForCard() {
+    this.concatinated += this.types[0].type.toLowerCase();
+    if (this.length > 1) {
+      this.concatinated += ' & mehr';
+    }
+  }
+
+  transformForDetails() {
+    for (let i = 0; i < this.length; i++) {
+      if (this.isLastElement(i)) {
+        this.concatinated += this.types[i].type.toLowerCase();
       } else {
-        concatTypes += types[i].type.toLowerCase() + ', ';
+        this.concatinated += this.types[i].type.toLowerCase() + ', ';
       }
     }
-    return concatTypes;
   }
 
-  isMoreThanOneMuseumType(origin: string): boolean {
-    return length > 1 && this.isMuseumsList(origin);
-  }
-
-  isMuseumsList(origin: string): boolean {
-    return origin === 'museumsList';
-  }
-
-  isThirdCuisine(elementIndex: number, origin: string): boolean {
-    return elementIndex === 2 && this.isRestaurantsList(origin);
-  }
-
-  isSecondCuisine(elementIndex: number, origin: string): boolean {
-    return elementIndex === 1  && this.isRestaurantsList(origin);
-  }
-
-  isRestaurantsList(origin: string): boolean {
-    return origin === 'restaurantsList';
-  }
-
-  isLastElement(elementIndex: number, arrayLength: number): boolean {
-    return elementIndex === (arrayLength - 1);
+  isLastElement(elementIndex: number): boolean {
+    return elementIndex === (this.length - 1);
   }
 }
