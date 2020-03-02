bc69127b13d1e231c661627c657bed69ad115d74
Yuliya Sheludyakova <julia.sheludyakova@gmail.com>
2020-03-02 02:31:52 +0100

    Refactored the show-types pipe, specified for museum types

--- frontend/src/app/entries-list/card/show-types-pipe/show-types.pipe.ts
+++ frontend/src/app/entries-list/card/show-types-pipe/show-types.pipe.ts
@@ -7,23 +7,49 @@ export class ShowTypesPipe implements PipeTransform {
 
   transform(types: any[], origin: string): string {
     let concatTypes: string = '';
-
-    if (!types) {return ''}
-
+    if (!types) {
+      return '';
+    }
     length = types.length;
-
     for(let i = 0; i < length; i++) {
-      if (i > 1 && origin !== 'detailPage') {
+      if (this.isMoreThanOneMuseumType(origin)) {
+        concatTypes += types[i].type.toLowerCase() + ' & mehr';
+        break;
+      }
+      if (this.isThirdCuisine(i, origin)) {
         concatTypes += ' & mehr';
         break;
       }
-      if (length === 1 || i === (length - 1) || (i === 1  && origin !== 'detailPage')) {
+      if (this.isSecondCuisine(i, origin) || this.isLastElement(i, length)) {
         concatTypes += types[i].type.toLowerCase();
       } else {
         concatTypes += types[i].type.toLowerCase() + ', ';
       }
     }
-
     return concatTypes;
+  }
+
+  isMoreThanOneMuseumType(origin: string): boolean {
+    return length > 1 && this.isMuseumsList(origin);
+  }
+
+  isMuseumsList(origin: string): boolean {
+    return origin === 'museumsList';
+  }
+
+  isThirdCuisine(elementIndex: number, origin: string): boolean {
+    return elementIndex === 2 && this.isRestaurantsList(origin);
+  }
+
+  isSecondCuisine(elementIndex: number, origin: string): boolean {
+    return elementIndex === 1  && this.isRestaurantsList(origin);
+  }
+
+  isRestaurantsList(origin: string): boolean {
+    return origin === 'restaurantsList';
+  }
+
+  isLastElement(elementIndex: number, arrayLength: number): boolean {
+    return elementIndex === (arrayLength - 1);
   }
 }
